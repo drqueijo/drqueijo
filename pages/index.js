4c7657fd27df03f1d493a86774f79606958fd863
drqueijo <silveriomat1234@gmail.com>
2022-04-23 03:40:42 -0300

    feat: gigachad pic

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,29 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { useEffect, useRef } from 'react'
+import { useState, useRef } from 'react'
 
 const audioProps = {
   controls: true,
   autoplay: true,
 }
 
+
 export default function Home() {
-  const audioRef = useRef(null)
+  const audioRef = useRef(null);
+  const [click, setClick] = useState(false);
 
   const playAudio = () => {
     const audio = new Audio('/audio.mp3');
     audio.play();
+    setClick(true)
   }
-
+  
   return (
     <div onClick={playAudio} className={styles.container}>
-        <img src="/download.png" />
+      {click ? <img src="/gigachad.webp" /> : <img src="/download.png" /> }   
     </div>
   )
 }
